3434e0aaae5d1150b2eedc9030508cbd3b004692
Simon Li <spli@dundee.ac.uk>
2018-06-21 14:39:44 +0100

    Only set random OAUTH2_PROXY_COOKIE_SECRET if not defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # https://github.com/bitly/oauth2_proxy/blob/v2.2/README.md
-export OAUTH2_PROXY_COOKIE_SECRET=`dd if=/dev/urandom count=1 bs=48 2>/dev/null | base64`
+if [ -z "$OAUTH2_PROXY_COOKIE_SECRET" ]; then
+  export OAUTH2_PROXY_COOKIE_SECRET=`dd if=/dev/urandom count=1 bs=48 2>/dev/null | base64`
+fi
 exec oauth2_proxy --http-address=http://:4180 "$@"
